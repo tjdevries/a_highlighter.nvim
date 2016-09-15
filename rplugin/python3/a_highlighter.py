d357e399e770da29c949dccb66070a2c148fc28f
TJ DeVries <tim.devries@dornerworks.com>
2016-09-15 13:07:04 -0400

    Fix a line indentation

--- rplugin/python3/a_highlighter.py
+++ rplugin/python3/a_highlighter.py
@@ -76,3 +76,26 @@ class HighlighterPlugin(object):
 
         for keyword in results.keys():
             self.nvim.command('call matchaddpos("GruvboxAqua", {0})'.format(str(results[keyword])))
+
+    # This is from chromatica.nvim
+    # This gives a pretty good idea about how to highlight I think.
+    # And I'm quite sure it's async
+    def _highlight(self, filename, lbegin=1, lend=-1):
+        """internal highlight function"""
+        _lbegin = lbegin
+        _lend = self.vimh.line("$") if lend == -1 else lend
+
+        buffer = self.__vim.current.buffer
+        tu = self.ctx[filename]["tu"]
+
+        self.profiler.start("_highlight")
+        syn_group = syntax.get_highlight(tu, buffer.name, _lbegin, _lend)
+
+        for hl_group in syn_group:
+            for pos in syn_group[hl_group]:
+                _row = pos[0] - 1
+                col_start = pos[1] - 1
+                col_end = col_start + pos[2]
+                buffer.add_highlight(hl_group, _row, col_start, col_end,\
+                        self.syntax_src_id, async=True)
+        self.profiler.stop()
